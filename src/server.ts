143dd49d9c7bfec4474c530953f1a04e5dce52c3
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2023-12-19 12:16:35 +0300

    feat: docx.js package and logic

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,36 @@
 import config from './common/config';
 import app from './app';
 
+import { IWordOrderData, wordOderCreate } from './utils/wordOderCreate';
+
 app.listen(config.PORT, () =>
   console.log(`App is running on ${config.API_URL}${config.PORT}`)
 );
+
+// const DATA: IWordOrderData[] = [
+//   {
+//     item: 1,
+//     title: "Поликарбонат",
+//     dimension: "м2",
+//     count: 3,
+//     price: 3.14,
+//     sum: 9.42,
+//     vatRate: 20,
+//     vatSum: 1.84,
+//     totalSum: 11.23,
+//   },
+//   {
+//     item: 2,
+//     title: "Труба профильная 40*40*2, ст.20, длинна 12 метров, без резки, верхняя загрзука",
+//     dimension: "шт",
+//     count: 1,
+//     price: 12313.14,
+//     sum: 11258.36,
+//     vatRate: 20,
+//     vatSum: 8572.36,
+//     totalSum: 113856.36,
+//   },
+
+// ];
+
+// wordOderCreate(DATA);
---
+++ src/utils/wordOderCreate.ts
@@ -0,0 +1,500 @@
+import * as fs from "fs";
+import path from 'path';
+import {
+    AlignmentType,
+    BorderStyle,
+    HeadingLevel,
+    Paragraph,
+    patchDocument,
+    PatchType,
+    Table,
+    TableCell,
+    TableRow,
+    TextDirection,
+    TextRun,
+    VerticalAlign,
+    WidthType
+} from "docx";
+
+export interface IWordOrderData {
+  item: number;
+  title: string;
+  dimension: string;
+  count: number;
+  price: number;
+  sum: number;
+  vatRate: number;
+  vatSum: number;
+  totalSum: number;
+}
+
+export const wordOderCreate = (data: IWordOrderData[]) => {
+  const font = "Times New Roman";
+  const generateRows = (prices: IWordOrderData[]): TableRow[] =>
+prices.map(
+  ({ item, title, dimension, count, price, sum, vatRate, vatSum, totalSum}) =>
+    new TableRow({
+      children: [
+        new TableCell({
+            children: [new Paragraph(item.toString())],
+            verticalAlign: VerticalAlign.CENTER,
+            textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+        new TableCell({
+            children: [new Paragraph(title)],
+            verticalAlign: VerticalAlign.CENTER,
+            textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+        new TableCell({
+          children: [new Paragraph({
+            text: dimension,
+            alignment: AlignmentType.CENTER
+          })],
+          verticalAlign: VerticalAlign.CENTER,
+          textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+        new TableCell({
+          children: [new Paragraph({
+            text: count.toString(),
+            alignment: AlignmentType.CENTER
+          })],
+          verticalAlign: VerticalAlign.CENTER,
+          textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+        new TableCell({
+          children: [new Paragraph({
+            text: price.toString(),
+            alignment: AlignmentType.CENTER
+          })],
+          verticalAlign: VerticalAlign.CENTER,
+          textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+        new TableCell({
+          children: [new Paragraph({
+            text: sum.toString(),
+            alignment: AlignmentType.CENTER
+          })],
+          verticalAlign: VerticalAlign.CENTER,
+          textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+        new TableCell({
+          children: [new Paragraph({
+            text: vatRate.toString(),
+            alignment: AlignmentType.CENTER
+          })],
+          verticalAlign: VerticalAlign.CENTER,
+          textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+        new TableCell({
+          children: [new Paragraph({
+            text: vatSum.toString(),
+            alignment: AlignmentType.CENTER
+          })],
+          verticalAlign: VerticalAlign.CENTER,
+          textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+        new TableCell({
+          children: [new Paragraph({
+            text: totalSum.toString(),
+            alignment: AlignmentType.CENTER
+          })],
+          verticalAlign: VerticalAlign.CENTER,
+          textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+        }),
+      ],
+  }),
+);
+
+patchDocument(fs.readFileSync(__dirname + path.sep + '../' + `assets` + path.sep + `template.docx`), {
+  patches: {
+    orderID: {
+        type: PatchType.PARAGRAPH,
+        children: [new TextRun("12345")],
+    },
+    orderDate: {
+        type: PatchType.PARAGRAPH,
+        children: [new TextRun(Date.now().toString())],
+    },
+    customerName: {
+        type: PatchType.PARAGRAPH,
+        children: [new TextRun("ОАО Пастовичи")],
+    },
+    // paragraph_replace: {
+    //     type: PatchType.DOCUMENT,
+    //     children: [
+    //         new Paragraph("Lorem ipsum paragraph"),
+    //         new Paragraph("Another paragraph"),
+    //         new Paragraph({
+    //             children: [
+    //                 new TextRun("This is a "),
+    //                 new ExternalHyperlink({
+    //                     children: [
+    //                         new TextRun({
+    //                             text: "Google Link",
+    //                         }),
+    //                     ],
+    //                     link: "https://www.google.co.uk",
+    //                 }),
+    //                 new ImageRun({ data: fs.readFileSync(__dirname + `\\assets\\image1.jpeg`), transformation: { width: 100, height: 100 } }),
+    //             ],
+    //         }),
+    //     ],
+    // },
+    // header_adjective: {
+    //     type: PatchType.PARAGRAPH,
+    //     children: [new TextRun("Delightful Header")],
+    // },
+    // footer_text: {
+    //     type: PatchType.PARAGRAPH,
+    //     children: [
+    //         new TextRun("replaced just as"),
+    //         new TextRun(" well"),
+    //         new ExternalHyperlink({
+    //             children: [
+    //                 new TextRun({
+    //                     text: "BBC News Link",
+    //                 }),
+    //             ],
+    //             link: "https://www.bbc.co.uk/news",
+    //         }),
+    //     ],
+    // },
+    // image_test: {
+    //     type: PatchType.PARAGRAPH,
+    //     children: [new ImageRun({ data: fs.readFileSync(__dirname + `\\assets\\image1.jpeg`), transformation: { width: 100, height: 100 } })],
+    // },
+    table: {
+      type: PatchType.DOCUMENT,
+      children: [
+        new Table({
+          width: {
+              size: 9070,
+              type: WidthType.AUTO,
+          },
+          rows: [
+            new TableRow({
+              children: [
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "№",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "Наименование товара",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                    
+                                }),
+                            ],
+                            // text: 'HI all',
+                            // leftTabStop: 0,
+                            // indent: {left: convertInchesToTwip(0.0)},
+                            // // spacing: { line: 240 },
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // margins: {left: 0},
+                    // textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "Единица измерения",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "Кол-во",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "Цена, руб",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "Сумма, руб",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "Ставка НДС, %",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "Сумма НДС, руб",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "Всего с НДС, руб",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+              ],
+            }),
+            ...generateRows(data),
+            new TableRow({
+              children: [
+                new TableCell({
+                  children: [new Paragraph('')],
+                  borders: {
+                    bottom: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                    left: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                    right: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                  },
+                }),
+                new TableCell({
+                  children: [new Paragraph('')],
+                  borders: {
+                    bottom: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                    left: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                    right: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                  },
+                }),
+                new TableCell({
+                  children: [new Paragraph('')],
+                  borders: {
+                    bottom: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                    left: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                    right: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                  },
+                }),
+                new TableCell({
+                  children: [new Paragraph('')],
+                  borders: {
+                    bottom: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                    left: {
+                      style: BorderStyle.NONE,
+                      color: 'FFFFFF'
+                    },
+                  },
+                }),
+                new TableCell({
+                  children: [
+                    new Paragraph({
+                      heading: HeadingLevel.HEADING_2,
+                      children: [
+                        new TextRun({
+                          text: "ИТОГО:",
+                          bold: true,
+                          size: 20,
+                          font,
+                        }),
+                      ],
+                      alignment: AlignmentType.RIGHT,
+                    }),
+                  ],
+                  verticalAlign: VerticalAlign.CENTER,
+                  // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                  children: [
+                    new Paragraph({
+                      heading: HeadingLevel.HEADING_2,
+                      children: [
+                        new TextRun({
+                            text: "126484.25",
+                            bold: true,
+                            size: 20,
+                            font,
+                        }),
+                      ],
+                        alignment: AlignmentType.CENTER,
+                    }),
+                  ],
+                  verticalAlign: VerticalAlign.CENTER,
+                  // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                  children: [new Paragraph('')],
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "456789.23",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+                new TableCell({
+                    children: [
+                        new Paragraph({
+                            heading: HeadingLevel.HEADING_2,
+                            children: [
+                                new TextRun({
+                                    text: "423658.11",
+                                    bold: true,
+                                    size: 20,
+                                    font,
+                                }),
+                            ],
+                            alignment: AlignmentType.CENTER,
+                        }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    },
+  },
+}).then((doc) => {
+  fs.writeFileSync(__dirname + path.sep + `MyDoc-11.docx`, doc);
+});
+
+};
